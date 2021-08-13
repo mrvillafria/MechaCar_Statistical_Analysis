e7a01fe7ac0dbe015b7a21d918a83c9da09241ff
Michelle Villafria <michellevillafria@gmail.com>
2021-08-13 15:41:52 -0500

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,19 @@
 library(dplyr)
 
+###### Deliverable 1 ######
+
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_mpg) #create linear model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_mpg)) #generate summary statistics
+
+
+
+###### Deliverable 2 ######
+
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance= var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance= var(PSI), SD=sd(PSI), .groups = 'keep')
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MechaCar_Statistical_Analysis
+
+
+## Linear Regression to Predict MPG
+
+Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+Vehicle Length and Ground Clearance contribute the least amount of variance to the mpg values
+
+Is the slope of the linear model considered to be zero? Why or why not?
+The slope is non-zero because Vehicle Length (6.267) and Ground Clearance (3.546) have non-zero coefficients
+
+Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
